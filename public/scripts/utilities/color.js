fcf216b2db088e6ba62ce7224c37cb081dbde227
MytsV <mytsvictoria@gmail.com>
2022-05-06 10:46:27 +0300

    Color class refactoring

--- public/scripts/utilities/color.js
+++ public/scripts/utilities/color.js
@@ -1,13 +1,15 @@
+const colorRange = 255;
+
 /*
 Class that represents color in RGBA format.
 Parameters r, g, b range from 0 to 255.
 Parameter alpha ranges from 0 to 255.
  */
-
-const range = 255;
-
 class Color {
   constructor(r, g, b, alpha = 255) {
+    const toCheck = [r, g, b, alpha];
+    toCheck.forEach((value) => checkRange(value));
+
     this.r = r;
     this.g = g;
     this.b = b;
@@ -17,33 +19,39 @@ class Color {
   //"Factory method" that creates color from given HEX value
   static fromHex(hexColor) {
     const radix = 16;
-    const noAlphaLength = 7;
 
-    const r = parseInt(hexColor.slice(1, 3), radix);
-    const g = parseInt(hexColor.slice(3, 5), radix);
-    const b = parseInt(hexColor.slice(5, 7), radix);
-    if (hexColor.length > noAlphaLength) {
-      const alpha = parseInt(hexColor.slice(7, 9), radix);
-      return new Color(r, g, b, alpha);
-    }
-    return new Color(r, g, b);
+    const expression = new RegExp('#(.{2})(.{2})(.{2})(.{0,2})');
+    const result = expression.exec(hexColor).slice(1, 5); //Getting second to fifth group
+    const values = result.map((value) => {
+      if (value !== '') return parseInt(value, radix);
+      else return colorRange;
+    });
+
+    return new Color(...values);
   }
 
+  //Refer to the link https://www.wikiwand.com/en/Alpha_compositing to get to know more about the algorithm
   blend(color) {
-    const newAlpha = color.alpha / range + this.alpha / range * (1 - color.alpha / range);
-    const getNewParameter = (parameterA, parameterB) => {
-      const numerator = parameterA * color.alpha / range + parameterB * (1 - color.alpha / range);
+    const alphaA = color.alpha / colorRange;
+    const alphaB = this.alpha / colorRange;
+
+    const newAlpha = alphaA + alphaB * (1 - alphaA);
+    const getParam = (parameterA, parameterB) => {
+      const numerator = parameterA * alphaA + parameterB * (1 - alphaA);
       return Math.floor(numerator / newAlpha);
     };
 
-    return new Color(getNewParameter(color.r, this.r), getNewParameter(color.g, this.g),
-      getNewParameter(color.b, this.b), newAlpha);
+    return new Color(getParam(color.r, this.r), getParam(color.g, this.g), getParam(color.b, this.b), newAlpha);
   }
 
   //Converts color to RGBA CSS format. Use when passing color to CSS style parameter
   toString() {
-    return `rgba(${this.r},${this.g},${this.b},${this.alpha / range})`;
+    return `rgba(${this.r},${this.g},${this.b},${this.alpha / colorRange})`;
   }
+}
+
+function checkRange(parameter) {
+  if (parameter < 0 || parameter > colorRange) throw Error(`Color parameter ${parameter} is not in range 0...${colorRange}`);
 }
 
 export { Color };
