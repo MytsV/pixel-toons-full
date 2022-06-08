5e707b12f2654f6f9c64e0958a31172e9525d81d
MytsV <mytsvictoria@gmail.com>
2022-06-09 02:21:14 +0300

    [core] started trying to implement imageData in GIF encoder...

--- public/scripts/utilities/gif_encoder.js
+++ public/scripts/utilities/gif_encoder.js
@@ -11,6 +11,7 @@ https://www.w3.org/Graphics/GIF/spec-gif89a.txt
 class GifEncoder {
   static #headerSize = 0x0D;
   static #trailerSize = 0x02;
+  static #localImageDescriptorSize = 0x0A;
 
   constructor() {
   }
@@ -21,41 +22,100 @@ class GifEncoder {
   Name | Size | Offset | Description
    */
   encode(frames) {
-    this.buffer = new Buffer(GifEncoder.#headerSize + GifEncoder.#trailerSize);
+    this.fileBuffer = new Buffer();
     this.#setHeader();
-    this.#setLogicalScreenDescriptor();
+    frames.forEach((frame) => this.#setFrame(frame));
     this.#setTerminator();
-    return this.buffer.data;
+    return this.fileBuffer.data;
   }
 
   #setHeader() {
+    const buffer = new Buffer(GifEncoder.#headerSize);
     //Signature | 3 bytes | 0x00 | 'GIF'
-    this.buffer.writeString('GIF', 0x00);
+    buffer.writeString('GIF', 0x00);
     //Version | 3 bytes | 0x03 | The version we use is 89a
-    this.buffer.writeString('89a', 0x03);
+    buffer.writeString('89a', 0x03);
+    this.#setLogicalScreenDescriptor(buffer);
+    this.#appendBuffer(buffer);
   }
 
-  #setLogicalScreenDescriptor() {
+  #setLogicalScreenDescriptor(buffer) {
     //Logical Screen Width | 2 bytes | 0x06 | Width of any frame
-    this.buffer.write16Integer(256, 0x06);
+    buffer.write16Integer(1, 0x06);
     //Logical Screen Height | 2 bytes | 0x08 | Height of any frame
-    this.buffer.write16Integer(256, 0x08);
+    buffer.write16Integer(1, 0x08);
     /*
     Packed fields | 1 byte | 0x0A | Contains following subfields of data:
-    Bit 0 - Global Color Table Flag - 0, not used
-    Bit 1-3 - Color Resolution - Number of bits in original palette
-    Bit 4 - Color Table Sort Flag - 0, not used
-    Bit 5-7 - Size of the Global Color Table - Not used
+    Bit 7 - Global Color Table Flag - 0, not used
+    Bit 4-6 - Color Resolution - Number of bits in original palette
+    Bit 3 - Color Table Sort Flag - 0, not used
+    Bit 0-2 - Size of the Global Color Table - Not used
      */
     const fields = 0b01110111;
-    this.buffer.writeInteger(BITS_IN_BYTE, fields, 0x0A);
+    buffer.writeInteger(BITS_IN_BYTE, fields, 0x0A);
 
     //Background Color | 1 byte | 0x0B | Skipped, not used
     //Aspect Ratio | 1 byte | 0x0C | Skipped, not used
   }
 
   #setTerminator() {
-    this.buffer.write16Integer(0x3B, 0x0D);
+    const buffer = new Buffer(GifEncoder.#trailerSize);
+    //Terminator | 2 bytes | 0x00 | Special value 3Bh
+    buffer.write16Integer(0x3B, 0x00);
+  }
+
+  #appendBuffer(buffer) {
+    const newArray = [...this.fileBuffer.data, ...buffer.data];
+    this.fileBuffer = new Buffer(new Uint8Array(newArray));
+  }
+
+  #setFrame(imageData) {
+    this.#setLocalImageDescriptor(imageData);
+    this.#setLocalColorTable(imageData);
+    this.#setImage(imageData);
+  }
+
+  #setLocalImageDescriptor(imageData) {
+    const buffer = new Buffer(GifEncoder.#localImageDescriptorSize);
+    //Separator | 1 byte | 0x00 | Special value 2Ch
+    buffer.writeInteger(BITS_IN_BYTE, 0x2C, 0x00);
+    //Left | 2 byte | 0x01 | X position
+    buffer.write16Integer(0x00, 0x01);
+    //Top | 2 byte | 0x03 | Y position
+    buffer.write16Integer(0x00, 0x03);
+    //Width | 2 byte | 0x05 | Width of the image in pixels
+    buffer.write16Integer(1, 0x05);
+    //Height | 2 byte | 0x07 | Height of the image in pixels
+    buffer.write16Integer(1, 0x07);
+    /*
+    Packed fields | 1 byte | 0x0A | Contains following subfields of data:
+    Bit 5-7 - Size of Local Color Table Entry - Number of bits per entry
+    Bit 3-4 - Reserved
+    Bit 2 - Sort Flag - 0, not sorted
+    Bit 1 - Interlace Flag - 0, not interlaced
+    Bit 0 - Local Color Table Flag - 1, always used
+     */
+    const fields = 0b00100001;
+    buffer.writeInteger(BITS_IN_BYTE, fields, 0x09);
+    this.#appendBuffer(buffer);
+  }
+
+  //Temporary implementation
+  #setLocalColorTable(imageData) {
+    const colorLength = 3;
+    const size = 2;
+    const buffer = new Buffer(colorLength * size + 1);
+    //Only black and white color
+    buffer.writeInteger(colorLength, 0x000, 0x00);
+    buffer.writeInteger(colorLength, 0xFFF, 0x03);
+    buffer.writeInteger(1, 0x02, 0x06);
+    this.#appendBuffer(buffer);
+  }
+
+  #setImage(imageData) {
+    const buffer = new Buffer(3);
+    buffer.writeArray([1, 0, 0]);
+    this.#appendBuffer(buffer);
   }
 }
 
--- public/scripts/utilities/lzw_compression.js
+++ public/scripts/utilities/lzw_compression.js
@@ -6,30 +6,40 @@ created by Abraham Lempel, Jacob Ziv, and Terry Welch.
 Pleaser refer to this link for details:
 https://en.wikipedia.org/wiki/Lempel%E2%80%93Ziv%E2%80%93Welch
  */
-const compress = (input) => {
-  const table = new Map();
-  const output = [];
-  for (let code = 0; code < ASCII_COUNT; code++) {
-    table.set(String.fromCharCode(code), code);
+class LZWCompressor {
+  constructor(codeSize) {
+    this.table = new Map();
+    this.codeSize = codeSize;
   }
-  let tableIndex = ASCII_COUNT;
 
-  let previous = input.charAt(0);
-  for (let pos = 1; pos < input.length; pos++) {
-    const current = input.charAt(pos);
-    const appended = previous + current;
-    if (table.has(appended)) {
-      previous = appended;
-    } else {
-      output.push(table.get(previous));
-      table.set(appended, tableIndex);
-      tableIndex++;
-      previous = current;
+  encode(data) {
+    this.data = data;
+  }
+
+  compress(input) {
+    const output = [];
+    for (let code = 0; code < ASCII_COUNT; code++) {
+      this.table.set(String.fromCharCode(code), code);
     }
+    let tableIndex = ASCII_COUNT;
+
+    let previous = input.charAt(0);
+    for (let pos = 1; pos < input.length; pos++) {
+      const current = input.charAt(pos);
+      const appended = previous + current;
+      if (this.table.has(appended)) {
+        previous = appended;
+      } else {
+        output.push(this.table.get(previous));
+        this.table.set(appended, tableIndex);
+        tableIndex++;
+        previous = current;
+      }
+    }
+    output.push(this.table.get(previous));
+
+    return output;
   }
-  output.push(table.get(previous));
+}
 
-  return output;
-};
-
-export { compress };
+export { LZWCompressor };
