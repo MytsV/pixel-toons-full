c5f3b537d3085bbc50f125ed045f887c58731b4e
MytsV <mytsvictoria@gmail.com>
2022-06-08 19:24:19 +0300

    [refactored] refactored basic header-only GIF encoder

--- public/scripts/core/ui_elements.js
+++ public/scripts/core/ui_elements.js
@@ -2,7 +2,7 @@ import { BmpEncoder, bmpVersions } from '../utilities/bmp_encoder.js';
 import * as conv from '../utilities/bytes_conversion.js';
 import { BucketFill, Eraser, Pencil, Pointer, Tool } from './tools.js';
 import { Color } from '../utilities/color.js';
-import { encode } from '../utilities/gif_encoder.js';
+import { GifEncoder } from '../utilities/gif_encoder.js';
 
 const HIDE_DISPLAY = 'none';
 const SHOW_DISPLAY = 'block';
@@ -106,9 +106,9 @@ export class FileMenu extends UiElement {
 
   static #exportImage(canvas) {
     const image = canvas.getJoinedImage();
-    const encoder = new BmpEncoder(bmpVersions.bmp32);
-    //const data = encoder.encode(image);
-    const data = encode();
+    //const encoder = new BmpEncoder(bmpVersions.bmp32);
+    const encoder = new GifEncoder();
+    const data = encoder.encode(image);
     conv.downloadLocalUrl(conv.bytesToUrl(data), 'image.gif');
   }
 
--- public/scripts/utilities/gif_encoder.js
+++ public/scripts/utilities/gif_encoder.js
@@ -1,34 +1,62 @@
 import { Buffer } from './buffer.js';
 
+const BITS_IN_BYTE = 8;
+
 /*
-GIF (Graphics Interchange Format) is a creation of CompuServe and is used
-to store multiple bitmap images in a single file
-for exchange between platforms and systems.
+GIF (Graphics Interchange Format) is used to store
+multiple bitmap images in a single file
 Please refer to this link for specification source:
-https://www.cs.albany.edu/~sdc/csi333/Fal07/Lect/L18/Summary
-I take word as 64 bits (for x64 architecture)
-This file implements GIF87a version
+https://www.w3.org/Graphics/GIF/spec-gif89a.txt
  */
+class GifEncoder {
+  static #headerSize = 0x0D;
+  static #trailerSize = 0x02;
 
-const encode = (images) => {
-  //First we have to convert images, cause another way we don't know the size...
-  const headerSize = 0x0D;
-  const trailerSize = 0x02;
-  const buffer = new Buffer(headerSize + trailerSize);
-  let offset = 0x00;
-  buffer.writeString('GIF', offset);
-  offset += 3;
-  buffer.writeString('87a', offset);
-  offset += 3;
-  buffer.write16Integer(256, offset);
-  offset += 2;
-  buffer.write16Integer(256, offset);
-  offset += 2;
-  buffer.writeArray([0b00101110], offset);
-  offset += 3;
-  buffer.write16Integer(0x3B, offset);
+  constructor() {
+  }
 
-  return buffer.data;
-};
+  /*
+  Creates a byte array which represents a file of GIF89a format.
+  The comment lines represent structures of format in a format:
+  Name | Size | Offset | Description
+   */
+  encode(frames) {
+    this.buffer = new Buffer(GifEncoder.#headerSize + GifEncoder.#trailerSize);
+    this.#setHeader();
+    this.#setLogicalScreenDescriptor();
+    this.#setTerminator();
+    return this.buffer.data;
+  }
 
-export { encode };
+  #setHeader() {
+    //Signature | 3 bytes | 0x00 | 'GIF'
+    this.buffer.writeString('GIF', 0x00);
+    //Version | 3 bytes | 0x03 | The version we use is 89a
+    this.buffer.writeString('89a', 0x03);
+  }
+
+  #setLogicalScreenDescriptor() {
+    //Logical Screen Width | 2 bytes | 0x06 | Width of any frame
+    this.buffer.write16Integer(256, 0x06);
+    //Logical Screen Height | 2 bytes | 0x08 | Height of any frame
+    this.buffer.write16Integer(256, 0x08);
+    /*
+    Packed fields | 1 byte | 0x0A | Contains following subfields of data:
+    Bit 0 - Global Color Table Flag - 0, not used
+    Bit 1-3 - Color Resolution - Number of bits in original palette
+    Bit 4 - Color Table Sort Flag - 0, not used
+    Bit 5-7 - Size of the Global Color Table - Not used
+     */
+    const fields = 0b01110111;
+    this.buffer.writeInteger(BITS_IN_BYTE, fields, 0x0A);
+
+    //Background Color | 1 byte | 0x0B | Skipped, not used
+    //Aspect Ratio | 1 byte | 0x0C | Skipped, not used
+  }
+
+  #setTerminator() {
+    this.buffer.write16Integer(0x3B, 0x0D);
+  }
+}
+
+export { GifEncoder };
