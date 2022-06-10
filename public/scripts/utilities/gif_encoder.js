6e7c8e2c97c9650a2ff0490e11d558ed237313a7
MytsV <mytsvictoria@gmail.com>
2022-06-10 17:08:34 +0300

    [core] refactored encoding of GIF blocks

--- public/scripts/utilities/gif_encoder.js
+++ public/scripts/utilities/gif_encoder.js
@@ -2,7 +2,7 @@ import { Buffer } from './buffer.js';
 import { LZWCompressor } from './lzw_compression.js';
 
 const BITS_IN_BYTE = 8;
-const MIN_COLOR_PARAMETERS = 3;
+const COLOR_PARAMETERS = 3;
 const MAX_COLOR_PARAMETERS = 4;
 
 /*
@@ -12,9 +12,13 @@ Please refer to this link for specification source:
 https://www.w3.org/Graphics/GIF/spec-gif89a.txt
  */
 class GifEncoder {
-  static #headerSize = 0x0D;
+  static #headerSize = 0x06;
+  static #logicalDescriptorSize = 0x07;
   static #trailerSize = 0x01;
   static #localImageDescriptorSize = 0x0A;
+  static #applicationExtensionSize = 19;
+  static #graphicsControlExtensionSize = 8;
+  #mainBuffer;
 
   constructor() {
   }
@@ -25,27 +29,29 @@ class GifEncoder {
   Name | Size | Offset | Description
    */
   encode(frames) {
-    this.fileBuffer = new Buffer();
-    this.#setHeader(frames[0]);
-    this.#setNetscape();
+    this.#mainBuffer = new Buffer();
+    this.#setHeader();
+    this.#setLogicalScreenDescriptor(GifEncoder.#getImageSize(frames));
+    this.#setApplicationExtension();
     frames.forEach((frame) => this.#setFrameData(frame));
     this.#setTerminator();
-    return this.fileBuffer.data;
+    return this.#mainBuffer.data;
   }
 
-  #setHeader(frame) {
+  #setHeader() {
     const buffer = new Buffer(GifEncoder.#headerSize);
     //Signature and version | 6 bytes | 0x00 | 'GIF89a'
     buffer.writeString('GIF89a');
-    this.#setLogicalScreenDescriptor(buffer, frame);
     this.#appendBuffer(buffer);
   }
 
-  #setLogicalScreenDescriptor(buffer, frame) {
+  #setLogicalScreenDescriptor({ width, height }) {
+    const buffer = new Buffer(GifEncoder.#logicalDescriptorSize);
+
     //Logical Screen Width | 2 bytes | 0x06 | Width of any frame
-    buffer.write16Integer(frame.width);
+    buffer.write16Integer(width);
     //Logical Screen Height | 2 bytes | 0x08 | Height of any frame
-    buffer.write16Integer(frame.height);
+    buffer.write16Integer(height);
     /*
     Packed fields | 1 byte | 0x0A | Contains following subfields of data:
     Bit 7 - Global Color Table Flag - 0, not used
@@ -58,12 +64,35 @@ class GifEncoder {
 
     //Background Color | 1 byte | 0x0B | Skipped, not used
     //Aspect Ratio | 1 byte | 0x0C | Skipped, not used
+
+    this.#appendBuffer(buffer);
   }
 
-  #setNetscape() {
-    const array = [0x21, 0xff, 0x0b, 0x4e, 0x45, 0x54, 0x53, 0x43, 0x41, 0x50, 0x45, 0x32, 0x2e, 0x30, 0x03, 0x01, 0x00, 0x00, 0x00];
-    const buffer = new Buffer(array.length);
-    buffer.writeArray(array);
+  static #getImageSize(frames) {
+    const firstFrame = frames[0];
+    return {
+      width: firstFrame.width,
+      height: firstFrame.height
+    };
+  }
+
+  #setApplicationExtension() {
+    const buffer = new Buffer(GifEncoder.#applicationExtensionSize);
+    //Extension introducer | 1 byte | 0x00 | Special value 21h
+    buffer.writeInteger(BITS_IN_BYTE, 0x21);
+    //Application Extension Label | 1 byte | 0x01 | Special value 0xFF
+    buffer.writeInteger(BITS_IN_BYTE, 0xFF);
+    //Byte size of block | 1 byte | 0x02 | Special value - 11 bytes
+    buffer.writeInteger(BITS_IN_BYTE, 0x0B);
+    //Special label | 11 bytes | 0x03 | NETSCAPE2.0
+    buffer.writeString('NETSCAPE2.0');
+    //Length of data sub-block | 1 byte | 0x0E | Special value - 3 bytes
+    buffer.writeInteger(BITS_IN_BYTE, 0x03);
+    //Data block values | 3 bytes | 0x0F | Special values
+    buffer.writeArray([0x01, 0x00, 0x00]);
+
+    //Block terminator | 1 byte | 0x10 | Special value 00h
+
     this.#appendBuffer(buffer);
   }
 
@@ -72,11 +101,6 @@ class GifEncoder {
     //Terminator | 2 bytes | 0x00 | Special value 3Bh
     buffer.writeInteger(BITS_IN_BYTE, 0x3B);
     this.#appendBuffer(buffer);
-  }
-
-  #appendBuffer(buffer) {
-    const newArray = [...this.fileBuffer.data, ...buffer.data];
-    this.fileBuffer = new Buffer(new Uint8Array(newArray));
   }
 
   #setFrameData(imageData) {
@@ -114,16 +138,36 @@ class GifEncoder {
   }
 
   #setGraphicControlExtension() {
-    const bufferTwo = new Buffer(8);
-    bufferTwo.writeArray([0x21, 0xF9, 0x04, 0x00, 0x0a, 0x00, 0x00, 0x00]);
-    this.#appendBuffer(bufferTwo);
+    const buffer = new Buffer(GifEncoder.#graphicsControlExtensionSize);
+    //Extension introducer | 1 byte | 0x00 | Special value 21h
+    buffer.writeInteger(BITS_IN_BYTE, 0x21);
+    //Graphic Control Label | 1 byte | 0x01 | Special value 0xF9
+    buffer.writeInteger(BITS_IN_BYTE, 0xF9);
+    //Byte size of block | 1 byte | 0x02 | Special value of 4 bytes
+    buffer.writeInteger(BITS_IN_BYTE, 0x04);
+    /*
+    Packed fields | 1 byte | 0x03 | Contains following subfields of data:
+    Bit 5-7 - Reserved
+    Bit 2-4 - Disposal Method - Not used
+    Bit 1 - User Input Flag - Don't wait for input
+    Bit 0 - Transparent Color Flag - Don't specify transparent color
+     */
+    const fields = 0b10000001;
+    buffer.writeInteger(BITS_IN_BYTE, fields);
+    //Delay time | 2 bytes | 0x04 | Time in 1/100 of seconds
+    buffer.write16Integer(0x0a);
+
+    //Transparent color index | 1 byte | 0x06 | Not used
+    //Block terminator | 1 byte | 0x07 | Special value 00h
+
+    this.#appendBuffer(buffer);
   }
 
   //Temporary
   #setLocalColorTable(imageData) {
     const table = GifEncoder.#getLocalColorTable(imageData);
-    const buffer = new Buffer(MIN_COLOR_PARAMETERS * table.length);
-    table.forEach((color) => buffer.writeInteger(MIN_COLOR_PARAMETERS * BITS_IN_BYTE, color));
+    const buffer = new Buffer(COLOR_PARAMETERS * table.length);
+    table.forEach((color) => buffer.writeInteger(COLOR_PARAMETERS * BITS_IN_BYTE, color));
     this.#appendBuffer(buffer);
   }
 
@@ -158,7 +202,7 @@ class GifEncoder {
     for (let i = 0; i < imageData.height; i++) {
       for (let j = imageData.width - 1; j >= 0; j--) {
         const dataPos = (i * imageData.width + j) * MAX_COLOR_PARAMETERS;
-        const color = imageData.data.slice(dataPos, dataPos + MIN_COLOR_PARAMETERS);
+        const color = imageData.data.slice(dataPos, dataPos + COLOR_PARAMETERS);
         const colorConverted = (color[2] !== 0 ? color[2] << 16 : color[2]) + (color[1] !== 0 ? color[1] << 8 : color[1]) + color[0];
         const index = table.indexOf(colorConverted);
         indices.set([index], i * imageData.width + j);
@@ -166,6 +210,13 @@ class GifEncoder {
     }
     return indices;
   }
+
+  #appendBuffer(buffer) {
+    const newArray = [...this.#mainBuffer.data, ...buffer.data];
+    this.#mainBuffer = new Buffer(new Uint8Array(newArray));
+  }
 }
+
+
 
 export { GifEncoder };
