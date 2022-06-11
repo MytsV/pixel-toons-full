7cca808b851470a1767b79c93c2d62dffff34094
MytsV <mytsvictoria@gmail.com>
2022-06-11 22:22:14 +0300

    [core] table functionality extracted to separate class

--- public/scripts/utilities/lzw_compression.js
+++ public/scripts/utilities/lzw_compression.js
@@ -1,7 +1,47 @@
 const MAX_BLOCK_SIZE = 255;
-const SPECIAL_CODE_COUNT = 2;
 const BITS_IN_BYTE = 8;
-const MAX_CODE_SIZE = 12;
+const MAX_TABLE_SIZE = 1 << /* Max code size */ 12;
+
+class CodeTable {
+  constructor(codeSize) {
+    this.colorBits = codeSize;
+    this.clearCode = 1 << this.colorBits;
+    this.endCode = this.clearCode + 1;
+    this.#clear();
+  }
+
+  getNewCode(previous, current) {
+    const appended = CodeTable.#getHash(previous, current);
+    const hasCode = this.table.has(appended);
+    return hasCode ? this.table.get(appended) : null;
+  }
+
+  set(previous, current) {
+    //Stop populating the table when its size exceeds limit
+    if (this.nextUnused >= MAX_TABLE_SIZE) return;
+
+    this.table.set(CodeTable.#getHash(previous, current), this.nextUnused++);
+
+    const size = 1 << this.codeSize;
+    /*
+    If the next entry will be bigger than the maximally supported one,
+    we increase the code size
+     */
+    if (this.nextUnused > size) {
+      this.codeSize++;
+    }
+  }
+
+  static #getHash(previous, current) {
+    return (previous << 8) | current;
+  }
+
+  #clear() {
+    this.table = new Map();
+    this.nextUnused = this.endCode + 1;
+    this.codeSize = this.colorBits + 1;
+  }
+}
 
 /*
 Lempel–Ziv–Welch (LZW) is a universal lossless data compression algorithm
@@ -11,34 +51,29 @@ https://en.wikipedia.org/wiki/Lempel%E2%80%93Ziv%E2%80%93Welch
  */
 class LZWCompressor {
   constructor(codeSize) {
-    this.minCodeSize = codeSize;
+    this.table = new CodeTable(codeSize);
     this.accumulant = '';
     this.blocks = [];
     this.currentBlock = [];
   }
 
   compress(input) {
-    this.#clearTable();
+    this.#addClearCode();
+
     let previous = input[0];
-    for (let pos = 1; pos < input.length; pos++) {
-      const current = input[pos];
-      const appended = (previous << 8) | current;
-      if (this.table.has(appended)) {
-        previous = this.table.get(appended);
+    for (const current of input.slice(1)) {
+      const newCode = this.table.getNewCode(previous, current);
+      if (newCode !== null) {
+        previous = newCode;
       } else {
         this.#output(previous);
-        if (this.tableIndex < 1 << MAX_CODE_SIZE) {
-          this.table.set(appended, this.tableIndex);
-          if (this.tableIndex > (2 ** this.codeSize - 1)) {
-            this.codeSize++;
-          }
-          this.tableIndex++;
-        }
+        this.table.set(previous, current);
         previous = current;
       }
     }
+
     this.#output(previous);
-    this.#addEOFCode();
+    this.#addEndCode();
     return this.blocks;
   }
 
@@ -63,26 +98,18 @@ class LZWCompressor {
 
   #byteToString(byte) {
     let string = byte.toString(2);
-    while (string.length < this.codeSize) {
+    while (string.length < this.table.codeSize) {
       string = '0' + string;
     }
     return string;
   }
 
   #addClearCode() {
-    this.#output(this.initialSize);
+    this.#output(this.table.clearCode);
   }
 
-  #clearTable() {
-    this.table = new Map();
-    this.initialSize = 2 ** this.minCodeSize;
-    this.tableIndex = this.initialSize + SPECIAL_CODE_COUNT;
-    this.codeSize = this.minCodeSize + 1;
-    this.#addClearCode();
-  }
-
-  #addEOFCode() {
-    this.#output(this.initialSize + 1);
+  #addEndCode() {
+    this.#output(this.table.endCode);
     if (this.currentBlock.length !== 0) {
       if (this.accumulant) {
         this.currentBlock.push(parseInt(this.accumulant, 2));
