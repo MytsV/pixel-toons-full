5c72c66aa9df216982ec43cbbac63819c6050e7e
MytsV <mytsvictoria@gmail.com>
2022-06-24 00:28:16 +0300

    [core] authentication - two forms added

--- public/scripts/authentication.js
+++ public/scripts/authentication.js
@@ -1,8 +1,12 @@
 import { authentication } from './utilities/database_handler.js';
 
+let mode = 'login';
+const DISPLAY_SHOW = 'block';
+const DISPLAY_HIDE = 'none';
+
 const register = async () => {
-  const emailElement = document.getElementById('email');
-  const passwordElement = document.getElementById('password');
+  const emailElement = document.getElementById('email-registration');
+  const passwordElement = document.getElementById('password-registration');
   const email = emailElement.value;
   const password = passwordElement.value;
   authentication.register(email, password, {}).then(() => {
@@ -13,8 +17,8 @@ const register = async () => {
 };
 
 const login = async () => {
-  const emailElement = document.getElementById('email');
-  const passwordElement = document.getElementById('password');
+  const emailElement = document.getElementById('email-login');
+  const passwordElement = document.getElementById('password-login');
   const email = emailElement.value;
   const password = passwordElement.value;
   await authentication.login(email, password).then(() => {
@@ -27,10 +31,26 @@ const login = async () => {
 window.onload = () => {
   const registerButton = document.getElementById('register');
   registerButton.onclick = () => {
-    register();
+    if (mode === 'register') {
+      register();
+    } else {
+      const registerForm = document.getElementById('registration-form');
+      registerForm.style.display = DISPLAY_SHOW;
+      const loginForm = document.getElementById('login-form');
+      loginForm.style.display = DISPLAY_HIDE;
+      mode = 'register';
+    }
   };
   const loginButton = document.getElementById('login');
   loginButton.onclick = () => {
-    login();
+    if (mode === 'login') {
+      login();
+    } else {
+      const loginForm = document.getElementById('login-form');
+      loginForm.style.display = DISPLAY_SHOW;
+      const registrationForm = document.getElementById('registration-form');
+      registrationForm.style.display = DISPLAY_HIDE;
+      mode = 'login';
+    }
   };
 };
