fe650aabb01ac695921c68a258c08cf284d440c9
MytsV <mytsvictoria@gmail.com>
2022-12-08 15:08:02 +0300

    [refactoring] LZW compression algorithm refactored

--- public/scripts/utilities/lzw_compression.js
+++ public/scripts/utilities/lzw_compression.js
@@ -1,26 +1,33 @@
-const compress = (data) => {
-  const table = new Map();
-  for (let i = 0; i < 256; i++) {
-    table.set(String.fromCharCode(i), i);
-  }
+const ASCII_COUNT = 256;
 
+/*
+Lempel–Ziv–Welch (LZW) is a universal lossless data compression algorithm
+created by Abraham Lempel, Jacob Ziv, and Terry Welch.
+Pleaser refer to this link for details:
+https://en.wikipedia.org/wiki/Lempel%E2%80%93Ziv%E2%80%93Welch
+ */
+const compress = (input) => {
+  const table = new Map();
   const output = [];
-  let element = data.charAt(0);
-  let index = 1;
-  let tableIndex = 256;
-  while (index < data.length) {
-    const c = data.charAt(index);
-    if (table.has(element + c)) {
-      element += c;
-    } else {
-      output.push(table.get(element));
-      table.set(element + c, tableIndex);
-      tableIndex++;
-      element = c;
-    }
-    index++;
+  for (let code = 0; code < ASCII_COUNT; code++) {
+    table.set(String.fromCharCode(code), code);
   }
-  output.push(table.get(element));
+  let tableIndex = ASCII_COUNT;
+
+  let previous = input.charAt(0);
+  for (let pos = 1; pos < input.length; pos++) {
+    const current = input.charAt(pos);
+    const appended = previous + current;
+    if (table.has(appended)) {
+      previous = appended;
+    } else {
+      output.push(table.get(previous));
+      table.set(appended, tableIndex);
+      tableIndex++;
+      previous = current;
+    }
+  }
+  output.push(table.get(previous));
 
   return output;
 };
