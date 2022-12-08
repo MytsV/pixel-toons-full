97233206c77beb80158e6b36a5e8c3707ed20b76
MytsV <mytsvictoria@gmail.com>
2022-12-08 19:24:19 +0300

    [refactored] refactored basic header-only GIF encoder

--- public/scripts/utilities/buffer.js
+++ public/scripts/utilities/buffer.js
@@ -15,22 +15,14 @@ class Buffer {
   }
 
   write16Integer(number, offset) {
-    this.#writeInteger(16, number, offset);
+    this.writeInteger(16, number, offset);
   }
 
   write32Integer(number, offset) {
-    this.#writeInteger(32, number, offset);
+    this.writeInteger(32, number, offset);
   }
 
-  write48Integer(number, offset) {
-    this.#writeInteger(48, number, offset);
-  }
-
-  write64Integer(number, offset) {
-    this.#writeInteger(64, number, offset);
-  }
-
-  #writeInteger(bits, number, offset) {
+  writeInteger(bits, number, offset) {
     const array = intToByteArray(number, bits / BITS_IN_BYTE);
     this.writeArray(array, offset);
   }
