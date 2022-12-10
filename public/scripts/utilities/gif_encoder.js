04db54c8d8d834c5014ec66000878b17d2b412cd
MytsV <mytsvictoria@gmail.com>
2022-12-10 18:41:39 +0300

    [core] refactored buffer and dependent modules

--- public/scripts/utilities/gif_encoder.js
+++ public/scripts/utilities/gif_encoder.js
@@ -1,7 +1,7 @@
 import { Buffer } from './buffer.js';
 import { LZWCompressor } from './lzw_compression.js';
 
-const BITS_IN_BYTE = 8;
+const EMPTY_VALUE = 0x00;
 const COLOR_PARAMETERS = 3;
 const MAX_COLOR_PARAMETERS = 4;
 
@@ -12,12 +12,6 @@ Please refer to this link for specification source:
 https://www.w3.org/Graphics/GIF/spec-gif89a.txt
  */
 class GifEncoder {
-  static #headerSize = 0x06;
-  static #logicalDescriptorSize = 0x07;
-  static #trailerSize = 0x01;
-  static #localImageDescriptorSize = 0x0A;
-  static #applicationExtensionSize = 19;
-  static #graphicsControlExtensionSize = 8;
   #mainBuffer;
 
   constructor() {
@@ -26,7 +20,7 @@ class GifEncoder {
   /*
   Creates a byte array which represents a file of GIF89a format.
   The comment lines represent structures of format in a format:
-  Name | Size | Offset | Description
+  Name | Size | Description
    */
   encode(frames) {
     this.#mainBuffer = new Buffer();
@@ -39,33 +33,29 @@ class GifEncoder {
   }
 
   #setHeader() {
-    const buffer = new Buffer(GifEncoder.#headerSize);
-    //Signature and version | 6 bytes | 0x00 | 'GIF89a'
-    buffer.writeString('GIF89a');
-    this.#appendBuffer(buffer);
+    //Signature and version | 6 bytes | GIF89a
+    this.#mainBuffer.writeString('GIF89a');
   }
 
   #setLogicalScreenDescriptor({ width, height }) {
-    const buffer = new Buffer(GifEncoder.#logicalDescriptorSize);
-
-    //Logical Screen Width | 2 bytes | 0x06 | Width of any frame
-    buffer.write16Integer(width);
-    //Logical Screen Height | 2 bytes | 0x08 | Height of any frame
-    buffer.write16Integer(height);
+    //Logical Screen Width | 2 bytes | Width of any frame
+    this.#mainBuffer.write16Integer(width);
+    //Logical Screen Height | 2 bytes | Height of any frame
+    this.#mainBuffer.write16Integer(height);
     /*
-    Packed fields | 1 byte | 0x0A | Contains following subfields of data:
+    Packed fields | 1 byte | Contains following subfields of data:
     Bit 7 - Global Color Table Flag - 0, not used
     Bit 4-6 - Color Resolution - Number of bits in original palette
     Bit 3 - Color Table Sort Flag - 0, not used
     Bit 0-2 - Size of the Global Color Table - Not used
      */
     const fields = 0b00000000;
-    buffer.writeInteger(BITS_IN_BYTE, fields);
+    this.#mainBuffer.writeByte(fields);
 
-    //Background Color | 1 byte | 0x0B | Skipped, not used
-    //Aspect Ratio | 1 byte | 0x0C | Skipped, not used
-
-    this.#appendBuffer(buffer);
+    //Background Color | 1 byte | Skipped, not used
+    this.#mainBuffer.writeByte(EMPTY_VALUE);
+    //Aspect Ratio | 1 byte | Skipped, not used
+    this.#mainBuffer.writeByte(EMPTY_VALUE);
   }
 
   static #getImageSize(frames) {
@@ -77,30 +67,26 @@ class GifEncoder {
   }
 
   #setApplicationExtension() {
-    const buffer = new Buffer(GifEncoder.#applicationExtensionSize);
-    //Extension introducer | 1 byte | 0x00 | Special value 21h
-    buffer.writeInteger(BITS_IN_BYTE, 0x21);
-    //Application Extension Label | 1 byte | 0x01 | Special value 0xFF
-    buffer.writeInteger(BITS_IN_BYTE, 0xFF);
-    //Byte size of block | 1 byte | 0x02 | Special value - 11 bytes
-    buffer.writeInteger(BITS_IN_BYTE, 0x0B);
-    //Special label | 11 bytes | 0x03 | NETSCAPE2.0
-    buffer.writeString('NETSCAPE2.0');
-    //Length of data sub-block | 1 byte | 0x0E | Special value - 3 bytes
-    buffer.writeInteger(BITS_IN_BYTE, 0x03);
-    //Data block values | 3 bytes | 0x0F | Special values
-    buffer.writeArray([0x01, 0x00, 0x00]);
+    //Extension introducer | 1 byte | Special value 21h
+    this.#mainBuffer.writeByte(0x21);
+    //Application Extension Label | 1 byte | Special value 0xFF
+    this.#mainBuffer.writeByte(0xFF);
+    //Byte size of block | 1 byte | Special value - 11 bytes
+    this.#mainBuffer.writeByte(0x0B);
+    //Special label | 11 bytes | NETSCAPE2.0
+    this.#mainBuffer.writeString('NETSCAPE2.0');
+    //Length of data sub-block | 1 byte | Special value - 3 bytes
+    this.#mainBuffer.writeByte(0x03);
+    //Data block values | 3 bytes | Special values
+    this.#mainBuffer.writeArray([0x01, 0x00, 0x00]);
 
-    //Block terminator | 1 byte | 0x10 | Special value 00h
-
-    this.#appendBuffer(buffer);
+    //Block terminator | 1 byte | Special value 00h
+    this.#mainBuffer.writeByte(EMPTY_VALUE);
   }
 
   #setTerminator() {
-    const buffer = new Buffer(GifEncoder.#trailerSize);
-    //Terminator | 2 bytes | 0x00 | Special value 3Bh
-    buffer.writeInteger(BITS_IN_BYTE, 0x3B);
-    this.#appendBuffer(buffer);
+    //Terminator | 1 byte | Special value 3Bh
+    this.#mainBuffer.writeByte(0x3B);
   }
 
   #setFrameData(imageData) {
@@ -111,21 +97,21 @@ class GifEncoder {
   }
 
   #setLocalImageDescriptor(imageData) {
-    const buffer = new Buffer(GifEncoder.#localImageDescriptorSize);
+    //Separator | 1 byte | Special value 2Ch
+    this.#mainBuffer.writeByte(0x2C);
 
-    //Separator | 1 byte | 0x00 | Special value 2Ch
-    buffer.writeInteger(BITS_IN_BYTE, 0x2C);
+    //Left | 2 byte | X position, skipped
+    this.#mainBuffer.write16Integer(EMPTY_VALUE);
+    //Top | 2 byte | Y position, skipped
+    this.#mainBuffer.write16Integer(EMPTY_VALUE);
 
-    //Left | 2 byte | 0x01 | X position, skipped
-    //Top | 2 byte | 0x03 | Y position, skipped
-
-    //Width | 2 byte | 0x05 | Width of the image in pixels
-    buffer.write16Integer(imageData.width, 0x05);
-    //Height | 2 byte | 0x07 | Height of the image in pixels
-    buffer.write16Integer(imageData.height);
+    //Width | 2 byte | Width of the image in pixels
+    this.#mainBuffer.write16Integer(imageData.width, 0x05);
+    //Height | 2 byte | Height of the image in pixels
+    this.#mainBuffer.write16Integer(imageData.height);
 
     /*
-    Packed fields | 1 byte | 0x0A | Contains following subfields of data:
+    Packed fields | 1 byte | Contains following subfields of data:
     Bit 7 - Local Color Table Flag - 1, always used
     Bit 6 - Interlace Flag - 0, not interlaced
     Bit 5 - Sort Flag - 0, not sorted
@@ -133,42 +119,38 @@ class GifEncoder {
     Bit 0-2 - Size of Local Color Table Entry - Number of bits per entry
      */
     const fields = 0b10000001;
-    buffer.writeInteger(BITS_IN_BYTE, fields);
-    this.#appendBuffer(buffer);
+    this.#mainBuffer.writeByte(fields);
   }
 
   #setGraphicControlExtension() {
-    const buffer = new Buffer(GifEncoder.#graphicsControlExtensionSize);
-    //Extension introducer | 1 byte | 0x00 | Special value 21h
-    buffer.writeInteger(BITS_IN_BYTE, 0x21);
-    //Graphic Control Label | 1 byte | 0x01 | Special value 0xF9
-    buffer.writeInteger(BITS_IN_BYTE, 0xF9);
-    //Byte size of block | 1 byte | 0x02 | Special value of 4 bytes
-    buffer.writeInteger(BITS_IN_BYTE, 0x04);
+    //Extension introducer | 1 byte | Special value 21h
+    this.#mainBuffer.writeByte(0x21);
+    //Graphic Control Label | 1 byte | Special value 0xF9
+    this.#mainBuffer.writeByte(0xF9);
+    //Byte size of block | 1 byte | Special value of 4 bytes
+    this.#mainBuffer.writeByte(0x04);
     /*
-    Packed fields | 1 byte | 0x03 | Contains following subfields of data:
+    Packed fields | 1 byte | Contains following subfields of data:
     Bit 5-7 - Reserved
     Bit 2-4 - Disposal Method - Not used
     Bit 1 - User Input Flag - Don't wait for input
     Bit 0 - Transparent Color Flag - Don't specify transparent color
      */
     const fields = 0b10000001;
-    buffer.writeInteger(BITS_IN_BYTE, fields);
-    //Delay time | 2 bytes | 0x04 | Time in 1/100 of seconds
-    buffer.write16Integer(0x0a);
+    this.#mainBuffer.writeByte(fields);
+    //Delay time | 2 bytes | Time in 1/100 of seconds
+    this.#mainBuffer.write16Integer(0x0a);
 
-    //Transparent color index | 1 byte | 0x06 | Not used
-    //Block terminator | 1 byte | 0x07 | Special value 00h
-
-    this.#appendBuffer(buffer);
+    //Transparent color index | 1 byte | Not used
+    this.#mainBuffer.writeByte(EMPTY_VALUE);
+    //Block terminator | 1 byte | Special value 00h
+    this.#mainBuffer.writeByte(EMPTY_VALUE);
   }
 
   //Temporary
   #setLocalColorTable(imageData) {
     const table = GifEncoder.#getLocalColorTable(imageData);
-    const buffer = new Buffer(COLOR_PARAMETERS * table.length);
-    table.forEach((color) => buffer.writeInteger(COLOR_PARAMETERS * BITS_IN_BYTE, color));
-    this.#appendBuffer(buffer);
+    table.forEach((color) => this.#mainBuffer.writeInteger(COLOR_PARAMETERS, color));
   }
 
   static #getLocalColorTable(imageData) {
@@ -185,15 +167,11 @@ class GifEncoder {
     const codeSize = 2;
     const compressor = new LZWCompressor(codeSize);
     const compressed = compressor.compress(indices);
-    const length = 2 + compressed.reduce((acc, block) => acc + block.length + 1, 0);
-    const buffer = new Buffer(length);
-    buffer.writeInteger(BITS_IN_BYTE, codeSize);
+    this.#mainBuffer.writeByte(codeSize);
     compressed.forEach((block) => {
-      buffer.writeArray([block.length, ...block]);
+      this.#mainBuffer.writeArray([block.length, ...block]);
     });
-    buffer.writeInteger(BITS_IN_BYTE, 0);
-
-    this.#appendBuffer(buffer);
+    this.#mainBuffer.writeByte(EMPTY_VALUE);
   }
 
   static #pixelsToIndices(imageData) {
@@ -209,11 +187,6 @@ class GifEncoder {
       }
     }
     return indices;
-  }
-
-  #appendBuffer(buffer) {
-    const newArray = [...this.#mainBuffer.data, ...buffer.data];
-    this.#mainBuffer = new Buffer(new Uint8Array(newArray));
   }
 }
 
