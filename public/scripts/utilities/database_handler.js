b2a7390440b909f1ae3e96fa7c90daf97ddb0fbc
MytsV <mytsvictoria@gmail.com>
2022-12-28 01:08:14 +0300

    [refactoring] one more refactoring of authentication

--- public/scripts/utilities/database_handler.js
+++ public/scripts/utilities/database_handler.js
@@ -4,27 +4,24 @@ const DEFAULT_INIT = { 'method': 'POST' };
 const ID_KEY = 'userId';
 
 class Authentication {
-  async register(email, password, userData) {
-    const params = Object.assign({}, { email, password }, userData);
+  async register(params) {
     const response = await requestData('accounts:signUp', params);
     if (!response.ok) throw Error(response.status.toString());
     await Authentication.#setCookies(response);
   }
 
-  async login(email, password) {
-    const params = Object.assign({}, { email, password });
+  async login(params) {
     const response = await requestData('accounts:signInWithPassword', params);
     if (!response.ok) throw Error(response.status.toString());
     await Authentication.#setCookies(response);
   }
 
   getId() {
-    return window.localStorage.getItem(ID_KEY)
+    return window.localStorage.getItem(ID_KEY);
   }
 
   static async #setCookies(response) {
     const data = await response.json();
-    console.log(data['idToken']);
     window.localStorage.setItem(ID_KEY, data['idToken']);
   }
 }
